1880c0ee14a86ee88ac05f6c9d1c0d1424a7a442
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-02-06 16:51:11 +0900

    add solution

--- src/main/scala/LeetCodeEasy.scala
+++ src/main/scala/LeetCodeEasy.scala
@@ -2,11 +2,14 @@ object Easy {
 
   def main(args: Array[String]): Unit = {
     val accounts: Array[Array[Int]] = Array(Array(1, 2, 3), Array(4, 5, 6))
-    val nums: List[Int] = List(1,2,3,4,5)
+    val nums: List[Int] = List(1, 2, 3, 4, 5)
+    val candies: List[Int] = List(2, 3, 5, 6, 1, 2)
+    val int = 3
     val address: String = "255.255.100.1"
     println(maxmunWealth(accounts))
     println(runningSum(nums))
     println(defangIPadder(address))
+    println(kidsWithCandies(candies, int))
   }
 
   def maxmunWealth(accounts: Array[Array[Int]]): Int = {
@@ -21,4 +24,15 @@ object Easy {
   def defangIPadder(address: String): String = {
     address.replace(".", "[.]")
   }
+
+  def kidsWithCandies(candies: List[Int], extraCandies: Int): List[Boolean] = {
+    candies.map(v =>
+      if (candies.max <= v + extraCandies) {
+        true
+      } else {
+        false
+      }
+    )
+  }
+
 }
