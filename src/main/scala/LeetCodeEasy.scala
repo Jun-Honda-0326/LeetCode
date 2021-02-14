0486fc84ee9bc22d1957520cd91f1ded72d3869e
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-02-14 18:40:31 +0900

    numIdenticalPairs

--- src/main/scala/LeetCodeEasy.scala
+++ src/main/scala/LeetCodeEasy.scala
@@ -3,14 +3,18 @@ object Easy {
   def main(args: Array[String]): Unit = {
     val accounts: Array[Array[Int]] = Array(Array(1, 2, 3), Array(4, 5, 6))
     val nums: List[Int] = List(1, 2, 3, 4, 5)
+    val nums2: List[Int] = List(1, 2, 3, 1, 1, 3)
     val candies: List[Int] = List(2, 3, 5, 6, 1, 2)
     val int = 3
     val address: String = "255.255.100.1"
+
     println(maxmunWealth(accounts))
     println(runningSum(nums))
     println(defangIPadder(address))
     println(kidsWithCandies(candies, int))
     println(shuffle(candies, int))
+    println(numIdenticalPairs(nums2))
+
   }
 
   def maxmunWealth(accounts: Array[Array[Int]]): Int = {
@@ -41,6 +45,16 @@ object Easy {
     val nums2 = nums.splitAt(n)._2.zipWithIndex
     val newNums = nums1 ++ nums2
     newNums.sortBy(v => v._2).map(x => x._1)
+  }
+
+  def numIdenticalPairs(nums: List[Int]): Int  = {
+    val nums1 = nums.zipWithIndex
+    val nums2 = nums.zipWithIndex.drop(1)
+    val numsTup = for {
+      num1 <- nums1
+      num2 <- nums2
+    } yield (num1,num2)
+    numsTup.filter(v => v._1._1 == v._2._1 & v._1._2 < v._2._2).length
   }
 
 }
